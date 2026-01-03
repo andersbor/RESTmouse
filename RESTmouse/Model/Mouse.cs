3a7df5b1535fe73312aca80c844f78ea8bcc42d9
andersbor <anbo@zealand.dk>
2026-01-03 16:19:27 +0100

    model class Mouse

---
+++ RESTmouse/Model/Mouse.cs
@@ -0,0 +1,9 @@
+﻿namespace RESTmouse.Model
+{
+    public class Mouse
+    {
+        public int Id { get; set; }
+        public bool Wireless { get; set; }
+        public string? Color { get; set; }
+    }
+}
