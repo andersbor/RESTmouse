6c4397bb92ee20d54621202884f6fc87581a4688
andersbor <anbo@zealand.dk>
2026-01-03 19:10:57 +0100

    mouse controller class

---
+++ RESTmouse/Controllers/MouseController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RESTmouse.Model;
+
+namespace RESTmouse.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MouseController : ControllerBase
+    {
+        private MouseRepository repository = new MouseRepository();
+
+        // TODO dependency injection
+
+        // GET: api/<MouseController>
+        [HttpGet]
+        public IEnumerable<Mouse> Get()
+        {
+            return repository.GetAll();
+        }
+
+        // GET api/<MouseController>/5
+        [HttpGet("{id}")]
+        // TODO status codes
+        public Mouse? Get(int id)
+        {
+            return repository.GetById(id);
+        }
+
+        // POST api/<MouseController>
+        [HttpPost]
+        public Mouse Post([FromBody] Mouse value)
+        {
+            Mouse mouse = repository.Add(value);
+            return mouse;
+        }
+
+        // DELETE api/<MouseController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            repository.Delete(id);
+        }
+    }
+}
